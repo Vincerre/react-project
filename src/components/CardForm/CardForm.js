8a6709730d74aa0b54488e3d392a37b9dd2ead9c
Patryk Vincerre <bazior.patryk@gmail.com>
2023-04-04 19:53:42 +0200

    data modification - task 3

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,14 +1,21 @@
-import styles from './CardForm.module.scss';
-import { useState } from 'react';
-import Button from './../Button/Button';
-import TextInput from './../TextInput/TextInput';
+import styles from "./CardForm.module.scss";
+import { useState } from "react";
+import Button from "./../Button/Button";
+import TextInput from "./../TextInput/TextInput";
+import { useDispatch } from "react-redux";
 
 const CardForm = (props) => {
-  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+  const columnId = props.columnId;
+
+  const [title, setTitle] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.action({ title: title }, props.columnId);
-    setTitle('');
+    dispatch({
+      type: "ADD_CARD",
+      payload: { title, columnId },
+    });
+    setTitle("");
   };
 
   return (
--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,21 +1,25 @@
-import styles from './ColumnForm.module.scss';
-import { useState } from 'react';
-import Button from '../Button/Button';
-import TextInput from '../TextInput/TextInput';
+import styles from "./ColumnForm.module.scss";
+import { useState } from "react";
+import Button from "../Button/Button";
+import TextInput from "../TextInput/TextInput";
+import { useDispatch } from "react-redux";
 
 const ColumnForm = (props) => {
-  const [title, setTitle] = useState('');
-  const [icon, setIcon] = useState('');
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [icon, setIcon] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.action({ title: title, icon: icon });
-    setTitle('');
-    setIcon('');
+    dispatch({ type: "ADD_COLUMN", payload: { title, icon } });
+    setTitle("");
+    setIcon("");
   };
 
   return (
     <form className={styles.columnForm} onSubmit={handleSubmit}>
-      Title: <TextInput value={title} onChange={(e) => setTitle(e.target.value)} />
+      Title:{" "}
+      <TextInput value={title} onChange={(e) => setTitle(e.target.value)} />
       Icon: <TextInput value={icon} onChange={(e) => setIcon(e.target.value)} />
       <Button>Add Column</Button>
     </form>
